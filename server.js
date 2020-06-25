99dba270c59b10d5b8c964ad413909484cd8e6da
junilHwang <tjsdlf4261@naver.com>
2020-06-25 21:27:33 +0900

    refactor: server에 모듈 시스템 적용

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const users = require('./route/users');
+
+app.use(express.json()); // request post data 파싱을 위함
+
+// view
+app.use('/', express.static('public'));
+
+// api 부분
+app.use(users); /* /api/user */
+                /* /api/users에 대한 라우터 */
+
+app.listen(8080, () => {
+  console.log('server start');
+})
