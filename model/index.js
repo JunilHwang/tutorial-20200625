1a380528922bf3751434216d5c2b3fea0ce4d212
junilHwang <tjsdlf4261@naver.com>
2020-06-25 21:27:53 +0900

    refactor: [server] model / route 추가

---
+++ model/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  users: [
+    {
+      name: '최우연',
+      age: 25,
+    },
+    {
+      name: '황준일',
+      age: 27,
+    }
+  ]
+}
---
+++ route/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router()
+const model = require('../model');
+
+router.get('/api/users', (request, response) => {
+  response.json(model.users);
+})
+
+router.get('/api/user/:index', (request, response) => {
+  response.json(model.users[request.params.index]);
+})
+
+router.post('/api/user', (request, response) => {
+  model.users.push(request.body)
+  response.send({
+    success: true,
+  })
+})
+
+module.exports = router;
