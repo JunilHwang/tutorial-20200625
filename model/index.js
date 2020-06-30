c64ce03560a3c0fc496de276c420042b58bf8b41
junilhwang <tjsdlf4261@naver.com>
2020-06-30 22:39:25 +0900

    feat: mysql connection 작업 완료

--- model/index.js
+++ model/index.js
@@ -1,12 +1,14 @@
-module.exports = {
-  users: [
-    {
-      name: '최우연',
-      age: 25,
-    },
-    {
-      name: '황준일',
-      age: 27,
-    }
-  ]
-}
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'junil',
+  database: 'junil321#@!'
+});
+
+const promisePool = () => connection.promise();
+
+const query = sql => promisePool().query(sql);
+const execute = (...params) => promisePool().execute(...params);
+
+module.exports = { connection, query, execute };
